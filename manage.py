60391b8da3791710d15020443867d53235625de8
Divyansh Kharbanda <55491427+dkarmy12@users.noreply.github.com>
2021-03-04 12:58:02 +0530

    Fixed manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import Course
 from flask_script import Shell,Manager
 from flask_migrate import Migrate, MigrateCommand
 
@@ -10,7 +9,7 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db,Course = Course)
+    return dict(app=app, db=db)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
